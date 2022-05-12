7c4a14437ed57a3ddbdeb3d59fb61279eb248673
박민준 <alswns6541@gmail.com>
2022-05-13 02:15:48 +0900

    Login, SignUp 추가 완료. 동작 순서는 MainActivity -> Login -> SignUp -> Login -> MainActivity

--- MainActivity/app/src/main/java/com/example/mainactivity/MainActivity.kt
+++ MainActivity/app/src/main/java/com/example/mainactivity/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.mainactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.example.mainactivity.databinding.ActivityMainBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private var firebaseAuth : FirebaseAuth? = null
+    private var mBinding: ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        firebaseAuth = Firebase.auth
+
+        setContentView(binding.root)
+
+        binding.imageView00.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+            firebaseAuth?.signOut()
+        }
     }
+
+    override fun onDestroy() { //
+        mBinding = null
+        super.onDestroy()
+    }
+
 }
