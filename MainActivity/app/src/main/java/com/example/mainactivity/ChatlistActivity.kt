62a23194aec28584660e7879bb01bf3fcc787790
박민준 <alswns6541@gmail.com>
2022-05-15 00:26:10 +0900

    임시저장

--- MainActivity/app/src/main/java/com/example/mainactivity/ChatlistActivity.kt
+++ MainActivity/app/src/main/java/com/example/mainactivity/ChatlistActivity.kt
@@ -4,17 +4,20 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.example.mainactivity.databinding.ActivityChatlistBinding
 import com.example.mainactivity.databinding.ActivityCreatechatBinding
 import com.example.mainactivity.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 
 class ChatlistActivity : AppCompatActivity() {
     private var firebaseAuth : FirebaseAuth? = null
     private var mBinding: ActivityChatlistBinding? = null
     private val binding get() = mBinding!!
+    private val firebaseData = FirebaseDatabase.getInstance().reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +26,10 @@ class ChatlistActivity : AppCompatActivity() {
         val curUser:User = intent.getSerializableExtra("curUser") as User
         Toast.makeText(this,"현재 사용자 : ${curUser.Id}\n현재 사용자 스팟 : ${curUser.location}\n사용자가 선택한 음식 : ${curUser.menu}",Toast.LENGTH_SHORT).show()
         setContentView(binding.root)
+        binding.spotTv.text = curUser.location
+        binding.menuTv.text = curUser.menu
+
+
+
     }
 }
