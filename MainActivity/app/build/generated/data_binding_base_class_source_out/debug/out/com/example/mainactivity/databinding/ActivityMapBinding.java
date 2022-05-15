85064ae26a4dc1915bf32a10387edeef8a9c35aa
dudwnssss <dudwnssss@naver.com>
2022-05-15 18:41:50 +0900

    유아이추추추가

--- MainActivity/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/mainactivity/databinding/ActivityMapBinding.java
+++ MainActivity/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/mainactivity/databinding/ActivityMapBinding.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.SearchView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
@@ -23,6 +24,9 @@ public final class ActivityMapBinding implements ViewBinding {
   private final ConstraintLayout rootView;
 
   @NonNull
+  public final ImageButton backbtn;
+
+  @NonNull
   public final Button button2;
 
   @NonNull
@@ -34,9 +38,11 @@ public final class ActivityMapBinding implements ViewBinding {
   @NonNull
   public final Toolbar toolbar3;
 
-  private ActivityMapBinding(@NonNull ConstraintLayout rootView, @NonNull Button button2,
-      @NonNull SearchView searchView, @NonNull TextView textView8, @NonNull Toolbar toolbar3) {
+  private ActivityMapBinding(@NonNull ConstraintLayout rootView, @NonNull ImageButton backbtn,
+      @NonNull Button button2, @NonNull SearchView searchView, @NonNull TextView textView8,
+      @NonNull Toolbar toolbar3) {
     this.rootView = rootView;
+    this.backbtn = backbtn;
     this.button2 = button2;
     this.searchView = searchView;
     this.textView8 = textView8;
@@ -70,6 +76,12 @@ public final class ActivityMapBinding implements ViewBinding {
     // This is done to optimize the compiled bytecode for size and performance.
     int id;
     missingId: {
+      id = R.id.backbtn;
+      ImageButton backbtn = ViewBindings.findChildViewById(rootView, id);
+      if (backbtn == null) {
+        break missingId;
+      }
+
       id = R.id.button2;
       Button button2 = ViewBindings.findChildViewById(rootView, id);
       if (button2 == null) {
@@ -94,8 +106,8 @@ public final class ActivityMapBinding implements ViewBinding {
         break missingId;
       }
 
-      return new ActivityMapBinding((ConstraintLayout) rootView, button2, searchView, textView8,
-          toolbar3);
+      return new ActivityMapBinding((ConstraintLayout) rootView, backbtn, button2, searchView,
+          textView8, toolbar3);
     }
     String missingId = rootView.getResources().getResourceName(id);
     throw new NullPointerException("Missing required view with ID: ".concat(missingId));
